1ea606989fbabc256bd7ee5834acd8c37ab7ca28
lzx <15133661446@163.com>
2020-12-01 23:11:14 +0800

    刘振雄第一次提交

---
+++ src/test/java/com/Test1.java
@@ -0,0 +1,43 @@
+package com;
+
+import com.baizhi.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+
+@SpringBootTest(classes = Application.class)
+public class Test1 {
+    @Autowired
+    private StringRedisTemplate str;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    //对key操作
+    public void test(){
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        str.keys("*").forEach(a-> System.out.println(a));
+        System.out.println(str.delete("name"));
+
+
+
+    }
+
+    
+    @Test
+    public void test2(){
+
+    }
+
+
+    @Test
+    public void test3(){
+
+    }
+
+
+
+}
